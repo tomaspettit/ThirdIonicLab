b053ab607e703a3aba4cf45eabc5f861933010c4
unknown <G00419414@atu.ie>
2024-04-15 18:48:44 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'StorageLab',
+  webDir: 'www'
+};
+
+export default config;
